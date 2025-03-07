3133942e1c8cc96673f29e40f47cad996ebfc094
vinayaksankaranarayanan <vinayak.sankara@iutlook.com>
2025-03-06 23:53:10 -0800

    more front-end changes

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -27,7 +27,7 @@ const Login = () => {
         setLoading(true);
 
         try {
-            const response = await fetch("/api/login", {
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/login`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ username, password }),
