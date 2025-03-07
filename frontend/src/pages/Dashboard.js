58f38bc3603927eec5ac3019b972949c206fe691
vinayaksankaranarayanan <vinayak.sankara@iutlook.com>
2025-03-06 23:11:38 -0800

    more front-end changes

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
 import { Container, Typography, Button, Box } from "@mui/material";
 import Calendar from "react-calendar";
